afac06bbc5b8749cc2b80f1e4a786b1d6cdcf7c6
Trong Hieu <hieuntctk42@gmail.com>
2020-12-13 21:37:22 +0700

    Design layout 🐱‍👤🐱‍👤🐱‍👤

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -12,8 +12,43 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI Calculator'),
       ),
-      body: Center(
-        child: Text('Body text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: Card(colour: Color(0xFF1D1E33))),
+                Expanded(child: Card(colour: Color(0xFF1D1E33))),
+              ],
+            ),
+          ),
+          Expanded(child: Card(colour: Color(0xFF1D1E33))),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: Card(colour: Color(0xFF1D1E33))),
+                Expanded(child: Card(colour: Color(0xFF1D1E33))),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  final Color colour;
+
+  Card({@required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(13.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
