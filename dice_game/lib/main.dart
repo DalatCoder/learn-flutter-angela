ea6923a7543e7d87dae88ef6cf281892e37cef5d
Trong Hieu <hieuntctk42@gmail.com>
2020-12-13 08:58:58 +0700

    Complete section 🍕🍕🍕

--- dice_game/lib/main.dart
+++ dice_game/lib/main.dart
@@ -23,6 +23,13 @@ class _DiceGameState extends State<DiceGame> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -30,21 +37,13 @@ class _DiceGameState extends State<DiceGame> {
         children: [
           Expanded(
             child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
-              },
+              onPressed: changeDiceFace,
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
-              },
+              onPressed: changeDiceFace,
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
