6e8c46531653c38dbc5bc601861d2d483155b530
Trong Hieu <hieuntctk42@gmail.com>
2020-12-13 13:39:11 +0700

    7 buttons

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -6,19 +6,65 @@ void main() {
 }
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click me'),
-            ),
+          child: Column(
+            children: [
+              FlatButton(
+                onPressed: () {
+                  player.play('note1.wav');
+                },
+                child: null,
+                color: Colors.red,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note2.wav');
+                },
+                child: null,
+                color: Colors.orange,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note3.wav');
+                },
+                child: null,
+                color: Colors.yellow,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note4.wav');
+                },
+                child: null,
+                color: Colors.green,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note5.wav');
+                },
+                child: null,
+                color: Colors.teal,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note6.wav');
+                },
+                child: null,
+                color: Colors.blue,
+              ),
+              FlatButton(
+                onPressed: () {
+                  player.play('note7.wav');
+                },
+                child: null,
+                color: Colors.purple,
+              ),
+            ],
           ),
         ),
       ),
