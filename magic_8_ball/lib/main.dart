36683d442aaca5ddd337c4d6faf0cb00f5de1710
Trong Hieu <hieuntctk42@gmail.com>
2020-12-13 09:31:24 +0700

    Final solution 🐱‍🏍🐱‍🏍🐱‍🏍

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,47 +1,44 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      appBar: AppBar(
-        title: Text('Magic 8 Ball'),
-        backgroundColor: Colors.teal.shade900,
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
       ),
-      body: MyApp(),
-    ),
-  ));
-}
+    );
 
-class MyApp extends StatefulWidget {
+class BallPage extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  int ballNumber = 1;
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
 
-  void handleOnImageClick() {
-    int rand = Random().nextInt(5) + 1;
-    setState(() {
-      ballNumber = rand;
-    });
-  }
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child: FlatButton(
-              onPressed: handleOnImageClick,
-              child: Image.asset('images/ball$ballNumber.png'),
-            ),
-          ),
-        ],
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
