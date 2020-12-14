0efd86b6dae9945e28efdbaf8ba01c0b68ee0854
Trong Hieu <hieuntctk42@gmail.com>
2020-12-14 21:15:36 +0700

    Make request and parse json data

--- clima_weather/lib/screens/loading_screen.dart
+++ clima_weather/lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -15,6 +17,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    String url =
+        'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=2fd5b8798feb79a6c02ffdcb95b8e867';
+
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+
+      int condition = decodedData['weather'][0]['id'];
+      double temperature = decodedData['main']['temp'];
+      String cityName = decodedData['name'];
+
+      print(condition);
+      print(temperature);
+      print(cityName);
+    } else {
+      print('Oops! Something went wrong! ' + response.statusCode.toString());
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -23,6 +47,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
+
     return Scaffold(
       body: Center(child: Text('Hello')),
     );
