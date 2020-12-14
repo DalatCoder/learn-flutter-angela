a1157d744bb2a61fd4ed8c8b1edfc4ed6d9c4d29
Trong Hieu <hieuntctk42@gmail.com>
2020-12-14 12:06:38 +0700

    Dart routes

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -183,11 +183,17 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          Container(
-            color: kBottomContainerBackgroundColor,
-            margin: EdgeInsets.only(top: 13.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/results');
+            },
+            child: Container(
+              child: Center(child: Text('CALCULATE')),
+              color: kBottomContainerBackgroundColor,
+              margin: EdgeInsets.only(top: 13.0),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           )
         ],
       ),
